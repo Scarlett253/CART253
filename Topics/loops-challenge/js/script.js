ef678ac343f3f98d3831402f86468c1f94ab8d6c
Scarlett253 <yvonnarriola@gmail.com>
2025-11-04 16:12:12 -0500

    step 4 finished, so FINISHEDDDD!!!!

--- Topics/loops-challenge/js/script.js
+++ Topics/loops-challenge/js/script.js
@@ -11,12 +11,21 @@ let x = 0;
 let y = 0;
 let strokeValue = 0;
 
+
 /**
  * Creates the canvas
  */
 function setup() {
     createCanvas(500, 500);
-    background("pink");
+
+    for (let x = 0; x <= width; x++) {
+        const shade = map(x, 0, width, 0, 255);
+
+        push();
+        stroke(255, 153, 204, shade);
+        line(x, 0, x, height);
+        pop();
+    }
 }
 
 /**
@@ -24,40 +33,6 @@ function setup() {
  * gradually lightening colour
  */
 function draw() {
-
-
-    // stroke(0);
-    // line(0, 0, 0, height);
-
-    // stroke(25);
-    // line(50, 0, 50, height);
-
-    // stroke(50);
-    // line(100, 0, 100, height);
-
-    // stroke(75);
-    // line(150, 0, 150, height);
-
-    // stroke(100);
-    // line(200, 0, 200, height);
-
-    // stroke(125);
-    // line(250, 0, 250, height);
-
-    // stroke(150);
-    // line(300, 0, 300, height);
-
-    // stroke(175);
-    // line(350, 0, 350, height);
-
-    // stroke(200);
-    // line(400, 0, 400, height);
-
-    // stroke(225);
-    // line(450, 0, 450, height);
-
-    // stroke(250);
-    // line(500, 0, 500, height);
 
     while (x < width) {
         line(x, 0, x, height);
