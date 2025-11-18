44c6cdc38af50621e150c185044efb6d484e409d
Scarlett253 <yvonnarriola@gmail.com>
2025-11-18 17:42:39 -0500

    finish menu, now need to add variations
    yay

--- Topics/variation-jam/js/script.js
+++ Topics/variation-jam/js/script.js
@@ -15,8 +15,13 @@
 /**Start state for a title and Game over screen*/
 //Menu screen
 let gameState = "menu";
-//Title image
+//Title text and instructions images
 let cookieEaterText;
+let instructions;
+let useMouse;
+let avoidObstacles;
+let enterDoors;
+let escapeMonster;
 
 //Gameover screen
 let gameOver;
@@ -27,19 +32,27 @@ let youWin;
 //Current score
 let score = 0;
 
+/** Monster settings */
+//Monster position
+let monsterX = 200;
+let monsterY = 200;
+
 /** Load sounds and images */
 function preload() {
     cookieEaterText = loadImage("assets/images/cookieEaterText.png");
+    instructions = loadImage("assets/images/instructions.png");
+    useMouse = loadImage("assets/images/useMouse.png");
+    avoidObstacles = loadImage("assets/images/avoidObstacles.png");
+    enterDoors = loadImage("assets/images/enterDoors.png");
+    escapeMonster = loadImage("assets/images/escapeMonster.png");
 }
 
 //Start game when ENTER is pressed
-// function enterPressed(event) {
-//     if (gameState === "title screen" && event.keyCode === 78) {
-//         gameState = "playing"
-//     }
-// }
-
-
+function keyPressed() {
+    if (gameState === "menu" && key === "Enter") {
+        gameState = "playing"
+    }
+}
 
 /**
  * Create canvas and set up monster and mouse positions
@@ -92,31 +105,48 @@ function menuScreen() {
 
     //Title text
     imageMode(CENTER);
-    image(cookieEaterText, width / 2, height - 550, 900, 150);
-    //Instructions text
-    textSize(18);
-    text("- Move the frog with your mouse", width / 2, height / 2 - 10);
-    text("- Click to launch the tongue", width / 2, height / 2 + 20);
-    text("- Eating ladybugs eliminates your strikes,", width / 2, height / 2 + 50);
-    text("but lowers your score", width / 2, height / 2 + 80);
-    text("- You lose if the score is -5", width / 2, height / 2 + 110);
-    text("- You lose if you get 3 strikes ", width / 2, height / 2 + 140);
-    text("- CATCH THE FLIES!!!", width / 2, height / 2 + 170);
+    image(cookieEaterText, width / 2, height - 600, 900, 150);
+    //Instructions
+    imageMode(CENTER);
+    image(instructions, width / 2, height - 490, 400, 50);
+    imageMode(CENTER);
+    image(useMouse, width / 2, height - 420, 390, 20);
+    imageMode(CENTER);
+    image(avoidObstacles, width / 2, height - 370, 275, 17);
+    imageMode(CENTER);
+    image(enterDoors, width / 2, height - 320, 380, 30);
+    imageMode(CENTER);
+    image(escapeMonster, width / 2, height - 260, 230, 20);
     pop();
 
 
 }
 
 // /**Draw background*/
-// function drawBackground() {
-//     background(300, 250, 200);
-// }
+function drawBackground() {
+    background(300, 150, 200);
+}
 
 // /** Monster set up */
 
-// //Draw monster
-// function drawMonster() {
-// }
+//Draw monster
+function drawMonster() {
+    //Body
+    push();
+    noStroke();
+    fill("#391be1ff");
+    ellipse(monsterX, monsterY, 150, 200);
+    pop();
+
+    //Eyes
+    push();
+    fill("#FFFFFF");
+    ellipse(monsterX - 25, monsterY - 20, 40, 30);
+    ellipse(monsterX + 25, monsterY - 20, 40, 30);
+    pop();
+
+
+}
 
 // //Move monster
 // function moveMonster() {
@@ -148,34 +178,43 @@ function menuScreen() {
 // }
 
 // /** Cookie */
-// function drawCookie() {
-//     //Cookie
-//     push();
-//     noStroke();
-//     noCursor();
-//     fill("#efc713ff");
-//     circle(mouseX, mouseY, 40);
-//     pop();
+function drawCookie() {
+    //Cookie
+    push();
+    noStroke();
+    noCursor();
+    fill("#efc713ff");
+    circle(mouseX, mouseY, 40);
+    pop();
 
-//     //Chocolate chips
-//     fill("#680C07");
-//     let chips = [
-//         [-12, -8], [10, -6], [-5, 6],
-//         [14, 4], [-15, 5], [2, -14],
-//         [6, 12]
-//     ];
+    //Chocolate chips
+    fill("#680C07");
+    let chips = [
+        [-12, -8], [10, -6], [-5, 6],
+        [14, 4], [-15, 5], [2, -14],
+        [6, 12]
+    ];
 
-//     for (let i = 0; i < chips.length; i++) {
-//         let dx = chips[i][0];
-//         let dy = chips[i][1];
-//         circle(mouseX + dx, mouseY + dy, 6);
-//     }
-// }
+    for (let i = 0; i < chips.length; i++) {
+        let dx = chips[i][0];
+        let dy = chips[i][1];
+        circle(mouseX + dx, mouseY + dy, 6);
+    }
+}
 
-// //Display score
-// function displayScore() {
+//Display score
+function displayScore() {
+    push();
+    textSize(40);
+    textAlign(320, 100);
+    textFont("Comic Sans Ms");
+    fill("#000000");
+    stroke("#FFFFFF");
+    strokeWeight(4);
+    text(score, width * 0.85, height * 0.1);
+    pop();
 
-// }
+}
 
 // /**Game over and You win set up */
 
