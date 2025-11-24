0f08ca90b76962562751d2f516bac4009857bd04
Scarlett253 <yvonnarriola@gmail.com>
2025-11-24 15:14:38 -0500

    Finish debugging!!!!
    Everything is working good now

--- Topics/variation-jam/js/script.js
+++ Topics/variation-jam/js/script.js
@@ -32,9 +32,24 @@ let youWin;
 //Current score
 let score = 0;
 
-/** Monster settings */
+/**Monster's settings */
+//Image
 let monsterImage;
-
+//monster
+const monster = {
+    body: {
+        x: 100,
+        y: 200,
+        size: 100,
+        speed: 3
+    }
+};
+//cookie
+const cookie = {
+    x: undefined, // mouseX
+    y: undefined, // mouseY
+    size: 40
+};
 
 
 /** Load sounds and images */
@@ -55,15 +70,6 @@ function keyPressed() {
     }
 }
 
-const monster = {
-    body: {
-        x: 100,
-        y: 200,
-        size: 100,
-        speed: 3
-    }
-};
-
 /**
  * Create canvas and set up monster and mouse positions
 */
@@ -83,15 +89,16 @@ function draw() {
     else if (gameState === "playing") {
 
         drawBackground();
+        moveCookie();
         drawCookie();
         moveMonster();
         drawMonster();
-        checkMonsterCollision();
-        drawObstacles();
-        checkObstaclesCollision();
-        drawDoors();
-        checkDoorsCollision();
-        displayScore();
+        // checkMonsterCollision();
+        // drawObstacles();
+        // checkObstaclesCollision();
+        // drawDoors();
+        // checkDoorsCollision();
+        // displayScore();
 
 
     }
@@ -115,60 +122,58 @@ function menuScreen() {
     imageMode(CENTER);
     image(cookieEaterText, width / 2, height - 600, 900, 150);
     //Instructions
-    imageMode(CENTER);
     image(instructions, width / 2, height - 490, 400, 50);
-    imageMode(CENTER);
     image(useMouse, width / 2, height - 420, 390, 20);
-    imageMode(CENTER);
     image(avoidObstacles, width / 2, height - 370, 275, 17);
-    imageMode(CENTER);
     image(enterDoors, width / 2, height - 320, 380, 30);
-    imageMode(CENTER);
     image(escapeMonster, width / 2, height - 260, 230, 20);
     pop();
-
-
-}
+};
 
 // /**Draw background*/
 function drawBackground() {
     background(300, 150, 200);
-}
+};
 
 /** Cookie */
+function moveCookie() {
+    cookie.x = mouseX;
+    cookie.y = mouseY;
+};
+
 function drawCookie() {
     //Cookie
     push();
-    noCursor();
     noStroke();
     fill("#efc713ff");
-    circle(mouseX, mouseY, 40);
+    ellipse(cookie.x, cookie.y, cookie.size);
+};
 
 
-    //Chocolate chips
-    fill("#680C07");
-    let chips = [
-        [-12, -8], [10, -6], [-5, 6],
-        [14, 4], [-15, 5], [2, -14],
-        [6, 12]
-    ];
+//     //Chocolate chips
+//     fill("#680C07");
+//     let chips = [
+//         [-12, -8], [10, -6], [-5, 6],
+//         [14, 4], [-15, 5], [2, -14],
+//         [6, 12]
+//     ];
 
-    for (let i = 0; i < chips.length; i++) {
-        let dx = chips[i][0];
-        let dy = chips[i][1];
-        circle(mouseX + dx, mouseY + dy, 6);
-    }
-    pop();
-}
+//     for (let i = 0; i < chips.length; i++) {
+//         let dx = chips[i][0];
+//         let dy = chips[i][1];
+//         circle(mouseX + dx, mouseY + dy, 6);
+//     }
+//     pop();
+// }
 
 // /** Monster set up */
 
 // //Move monster
 function moveMonster() {
-    let followSpeed = 0.3;
+    let followSpeed = 0.05;
     monster.body.x = lerp(monster.body.x, mouseX, followSpeed);
     monster.body.y = lerp(monster.body.y, mouseY, followSpeed);
-}
+};
 
 //Draw monster
 function drawMonster() {
@@ -184,62 +189,60 @@ function drawMonster() {
     ellipse(monster.body.x - 20, monster.body.y - 20, 20, 30);
     ellipse(monster.body.x + 20, monster.body.y - 20, 20, 30);
     pop();
-
-
-}
+};
 
 
 
-//Monster collision
-function checkMonsterCollision() {
-}
+// //Monster collision
+// function checkMonsterCollision() {
+// }
 
-/** Obstacles and doors set up */
-//Draw Obstacles
-function drawObstacles() {
+// /** Obstacles and doors set up */
+// //Draw Obstacles
+// function drawObstacles() {
 
-}
+// }
 
-//Check obstacles collision
-function checkObstaclesCollision() {
+// //Check obstacles collision
+// function checkObstaclesCollision() {
 
-}
+// }
 
-//Draw doors
-function drawDoors() {
+// //Draw doors
+// function drawDoors() {
 
-}
+// }
 
-//Check doors collision
-function checkDoorsCollision() {
+// //Check doors collision
+// function checkDoorsCollision() {
 
-}
+// }
 
-//Display score
-function displayScore() {
-    push();
-    textSize(40);
-    textAlign(320, 100);
-    textFont("Comic Sans Ms");
-    fill("#000000");
-    stroke("#FFFFFF");
-    strokeWeight(4);
-    text(score, width * 0.85, height * 0.1);
-    pop();
+// //Display score
+// function displayScore() {
+//     push();
+//     textSize(40);
+//     textAlign(320, 100);
+//     textFont("Comic Sans Ms");
+//     fill("#000000");
+//     stroke("#FFFFFF");
+//     strokeWeight(4);
+//     text(score, width * 0.85, height * 0.1);
+//     pop();
 
-}
+// }
 
 
 
 
 
-/**Game over and You win set up */
+// /**Game over and You win set up */
 
-//Game over screen
-function gameOver() {
+// //Game over screen
+// function gameOver() {
 
-}
-//You win screen
-function youWin() {
+// }
+// //You win screen
+// function youWin() {
 
-}
+// }
