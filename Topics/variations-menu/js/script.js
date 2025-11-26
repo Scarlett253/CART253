df4d9cf28ee40c1477fe7112e8b27e5a0d03a39d
Scarlett253 <yvonnarriola@gmail.com>
2025-11-26 11:25:43 -0500

    Created a menu

--- Topics/variations-menu/js/script.js
+++ Topics/variations-menu/js/script.js
@@ -15,7 +15,7 @@ let state = "menu";
  * Create the canvas
 */
 function setup() {
-    createCanvas(500, 500);
+    createCanvas(1300, 700);
 }
 
 
@@ -27,56 +27,32 @@ function draw() {
         case "menu":
             menuDraw();
             break;
-        case "red-variation":
-            redDraw();
+        case "normal-variation":
+            normalDraw();
             break
-        case "green-variation":
-            greenDraw();
+        case "dog-variation":
+            dogDraw();
             break;
-        case "blue-variation":
-            blueDraw();
+        case "cat-variation":
+            catDraw();
             break;
     }
 }
 
-/**
- * Listen for mouse pressed and call the function for it in the
- * current state
- */
-function mousePressed() {
-    switch (state) {
-        case "menu":
-            menuMousePressed();
-            break;
-        case "red-variation":
-            redMousePressed();
-            break
-        case "green-variation":
-            greenMousePressed();
-            break;
-        case "blue-variation":
-            blueMousePressed();
-            break;
-    }
-}
 
 /**
  * Listen for keypressed and call the function for it in the
  * current state
  */
 function keyPressed(event) {
+    // ESC → go back to main menu
+    if (event.keyCode === 27) {
+        window.location.reload();
+    }
+
     switch (state) {
         case "menu":
             menuKeyPressed(event);
-            break;
-        case "red-variation":
-            redKeyPressed(event);
-            break
-        case "green-variation":
-            greenKeyPressed(event);
-            break;
-        case "blue-variation":
-            blueKeyPressed(event);
             break;
     }
 }
