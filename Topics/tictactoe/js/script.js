1578fc8aadf4378d2505b1500fcfd014a7ad5083
Scarlett253 <yvonnarriola@gmail.com>
2026-01-16 17:17:10 -0500

    created lines, already overwhelmed
    will continue tomorrow :)

--- Topics/tictactoe/js/script.js
+++ Topics/tictactoe/js/script.js
@@ -9,8 +9,11 @@
 
 /**
 */
+
+let coordinatesX = [50, 200, 350];
+let coordinatesY = [50, 200, 350];
 function setup() {
-    createCanvas(500, 500);
+    createCanvas(800, 500);
 
 }
 
@@ -19,6 +22,24 @@ function setup() {
  * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
 */
 function draw() {
-    background("#5d9be2ff")
+    // set a color 
+    background("#5d9be2ff");
+
+    /**
+     * Grid settings
+     */
+
+    //style lines
+    stroke("black");
+    strokeWeight(5);
+
+    //draw grid
+    line(coordinatesX[1], 50, coordinatesX[1], 450);
+    line(coordinatesX[2], 50, coordinatesX[2], 450);
+    line(50, coordinatesY[1], 500, coordinatesY[1]);
+    line(50, coordinatesY[2], 500, coordinatesY[2]);
+}
+
+function score() {
 
 }
