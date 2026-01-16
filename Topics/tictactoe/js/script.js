e7ac0303de3408f83c4834de7f76d45dfa0da495
Scarlett253 <yvonnarriola@gmail.com>
2026-01-16 16:52:57 -0500

    Changed folder, starting script

---
+++ Topics/tictactoe/js/script.js
@@ -0,0 +1,24 @@
+/**
+ * TicTacToe variation
+ * Yvonne Scarlett Arriola Lerin
+ * 
+ * A TicTacToe variation using my pets 
+ */
+
+"use strict";
+
+/**
+*/
+function setup() {
+    createCanvas(500, 500);
+
+}
+
+
+/**
+ * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+*/
+function draw() {
+    background("#5d9be2ff")
+
+}
